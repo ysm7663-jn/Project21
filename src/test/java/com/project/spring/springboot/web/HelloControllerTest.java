4eb2774dad473143198e7ca8bf9ed487b955e852
ysm7663-jn <ysm7663@gmail.com>
2021-10-21 23:52:32 +0900

    Controller

---
+++ src/test/java/com/project/spring/springboot/web/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.project.spring.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))  // 왜 인지는 모르겠지만 자동 import 가 안 돼서 직접 입력함
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
