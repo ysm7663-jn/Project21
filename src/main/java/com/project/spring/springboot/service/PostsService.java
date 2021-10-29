77e83cb7f2d71c0e09fc43a3bf32f9ffd7922598
ysm7663-jn <ysm7663@gmail.com>
2021-10-30 00:55:29 +0900

    PostsService

---
+++ src/main/java/com/project/spring/springboot/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.project.spring.springboot.service;
+
+import com.project.spring.springboot.domain.posts.PostsRepository;
+import com.project.spring.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
