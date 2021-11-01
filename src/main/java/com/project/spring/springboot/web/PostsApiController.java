7cb68314bcd53c3b75653d04ba616a1d6f7f9a9a
ysm7663-jn <ysm7663@gmail.com>
2021-11-01 22:36:14 +0900

    PostsApiController +update, findById

--- src/main/java/com/project/spring/springboot/web/PostsApiController.java
+++ src/main/java/com/project/spring/springboot/web/PostsApiController.java
@@ -1,9 +1,14 @@
 package com.project.spring.springboot.web;
 
 import com.project.spring.springboot.service.PostsService;
+import com.project.spring.springboot.web.dto.PostsResponseDto;
 import com.project.spring.springboot.web.dto.PostsSaveRequestDto;
+import com.project.spring.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +25,16 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update (@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 
 }
