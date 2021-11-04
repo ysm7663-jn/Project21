89acc5cbad32b02ecd6fef15f112d88439591b16
ysm7663-jn <ysm7663@gmail.com>
2021-11-04 22:58:05 +0900

    JPA Auditing 기능 추가

---
+++ src/main/java/com/project/spring/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.project.spring.springboot.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass   // 1
+@EntityListeners(AuditingEntityListener.class)  // 2
+public abstract class BaseTimeEntity {
+
+    @CreatedDate    // 3
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate   // 4
+    private LocalDateTime modifiedDate;
+
+}
