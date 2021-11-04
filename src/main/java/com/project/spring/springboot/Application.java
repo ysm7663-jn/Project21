42b4a2fbeab43010ec1cf8b35704b663bffb89d1
ysm7663-jn <ysm7663@gmail.com>
2021-11-04 22:58:01 +0900

    JPA Auditing 기능 추가

--- src/main/java/com/project/spring/springboot/Application.java
+++ src/main/java/com/project/spring/springboot/Application.java
@@ -2,7 +2,9 @@ package com.project.spring.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing  // JPA Auditing 기능 활성화
 @SpringBootApplication
 public class Application {
     public static void main (String[] args) {
