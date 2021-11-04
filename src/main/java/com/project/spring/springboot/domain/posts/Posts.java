f08fcf68811e1665280a9fec685385deb23f87b8
ysm7663-jn <ysm7663@gmail.com>
2021-11-04 22:58:08 +0900

    JPA Auditing 기능 추가

--- src/main/java/com/project/spring/springboot/domain/posts/Posts.java
+++ src/main/java/com/project/spring/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.project.spring.springboot.domain.posts;
 
+import com.project.spring.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter // 6
 @NoArgsConstructor // 5
 @Entity // 1
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 2
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 3
