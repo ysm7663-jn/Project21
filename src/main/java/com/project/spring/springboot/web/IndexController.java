1e66c5f4453813565c6acebb8c964397a86b0145
ysm7663-jn <ysm7663@gmail.com>
2021-11-09 00:42:16 +0900

    IndexController

--- src/main/java/com/project/spring/springboot/web/IndexController.java
+++ src/main/java/com/project/spring/springboot/web/IndexController.java
@@ -1,8 +1,10 @@
 package com.project.spring.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
