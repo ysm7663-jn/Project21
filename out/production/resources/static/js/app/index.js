657e6ce2e608550c6c75a5985a673158e16993b9
ysm7663-jn <ysm7663@gmail.com>
2021-11-09 14:57:24 +0900

    index.js∏

--- out/production/resources/static/js/app/index.js
+++ out/production/resources/static/js/app/index.js
@@ -5,6 +5,11 @@ var main = {
             $('#btn-save').on('click', function () {
                 _this.save();
                 });
+
+            // 수정
+            $('#btn-update').on('click', function () {
+                _this.update();
+            });
         },
 
         save : function () {
@@ -24,6 +29,28 @@ var main = {
                 alert('글이 등록되었습니다.');
                 window.location.href = '/';     // 글 등록 성곡시 메인페이지(/)로 이동
                 }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+        },
+
+        update : function () {
+            var data = {
+                title: $('#title').val(),
+                content: $('#content').val()
+            };
+
+            var id = $('#id').val();
+
+            $.ajax({
+                type: 'PUT',
+                url: '/api/v1/posts/' + id,
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).done(function () {
+                alert('글이 수정되었습니다.');
+                window.location.href = '/';
+            }).fail(function (error) {
                 alert(JSON.stringify(error));
             });
         }
