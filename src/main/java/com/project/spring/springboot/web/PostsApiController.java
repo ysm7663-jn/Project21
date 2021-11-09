4543d4b7ce5c75c48d449c67fae73cce8a6c860d
ysm7663-jn <ysm7663@gmail.com>
2021-11-09 14:37:36 +0900

    PostsApiController

--- src/main/java/com/project/spring/springboot/web/PostsApiController.java
+++ src/main/java/com/project/spring/springboot/web/PostsApiController.java
@@ -22,17 +22,17 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")   // 등록
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
-    @PutMapping("/api/v1/posts/{id}")
+    @PutMapping("/api/v1/posts/{id}")   // 수정
     public Long update (@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
+    @GetMapping("/api/v1/posts/{id}")   // 조회
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsService.findById(id);
     }
