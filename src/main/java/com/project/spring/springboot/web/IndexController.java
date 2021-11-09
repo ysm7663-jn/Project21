c2a6f4bcc2fa9b4f8447876377f4228c877e1589
ysm7663-jn <ysm7663@gmail.com>
2021-11-09 12:00:44 +0900

    IndexController - 조회 추가

--- src/main/java/com/project/spring/springboot/web/IndexController.java
+++ src/main/java/com/project/spring/springboot/web/IndexController.java
@@ -1,15 +1,20 @@
 package com.project.spring.springboot.web;
 
+import com.project.spring.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping
-    public String index() {
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
